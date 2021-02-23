5bd144b490b1d3266cb9397bd8cc74c9868c0c34
Mert Cobanov <mertcobanov@gmail.com>
2021-02-23 12:48:37 +0300

    bug fix

--- app/database/db_utils.py
+++ app/database/db_utils.py
@@ -4,33 +4,25 @@ from datetime import datetime
 DB_NAME = "test"
 COLLECTION = "entries"
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
 
 def connect_db(db_name=DB_NAME):
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client = pymongo.MongoClient("mongodb://mongodb:27017/")
     db = client[db_name]
 
     print("DB List:", client.list_database_names())
     return db
 
 
-def write_data(db, collection, entry):
+def write_data(db, collection):
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+
+    entry = {"time": current_time, "message": "hello world!",
+             "message_id": 1, "author": "Cobanov"}
     db[collection].insert_one(entry)
 
 
 def retrieve_data(db, collection):
     data = db[collection].find()
     return data
-
-
-# mydb = connect_db(DB_NAME)
-
-# entry_first = {"time": current_time, "message": "hello world!",
-#                "message_id": 1, "author": "Cobanov"}
-
-# write_data(mydb, "entries", entry_first)
-
-
-#entries_db = mydb["entries"]
